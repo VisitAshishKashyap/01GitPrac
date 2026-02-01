11065b1874de544f615118ae51b0439c3feaceb3
Ashish Kashyap <ashishsince1988@gmail.com>
2026-02-01 13:00:39 +0530

    added new function in index.js file

--- 01/index.js
+++ 01/index.js
@@ -4,3 +4,18 @@ function add(a,b){
 add(2,5)
 add(10,23)
 add(40,42)
+
+function sub(a,b){
+    return a-b
+}
+
+sub(10,5)
+sub(12,4)
+
+function mul(a,b){
+    return a*b
+}
+
+mul(2,3)
+mul(4,5)
+
